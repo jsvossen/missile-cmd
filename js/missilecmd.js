dcd7044b0fb9ade3f25c3a3ff5daaa1f663a0842
shasta28 <jessahafer-zdral@Jessas-MacBook-Pro.local>
2016-05-28 13:14:10 -0400

    animate lines

--- js/missilecmd.js
+++ js/missilecmd.js
@@ -7,20 +7,35 @@
         var canvasElement = $("<canvas width='" + CANVAS_WIDTH + "' height='" + CANVAS_HEIGHT + "'></canvas>");
 		var canvas = canvasElement.get(0).getContext("2d");
 
+		var userMissiles = []
+
 		function Missile(toX,toY) {
 			this.toX = toX;
 			this.toY = toY;
 			this.oX = CANVAS_WIDTH/2;
 			this.oY = CANVAS_HEIGHT;
+			this.amount = 0;
 		}
 
 		Missile.prototype.draw = function() {
 			var c = canvas;
+			this.amount += 0.05;
+			if (this.amount > 1) this.amount = 1;
 			c.moveTo(this.oX,this.oY);
-			c.lineTo(this.toX,this.toY);
+			dx = this.oX + (this.toX - this.oX) * this.amount;
+			dy = this.oY + (this.toY - this.oY) * this.amount;
+			c.lineTo(dx,dy);
 			c.strokeStyle = "dodgerblue";
 			c.lineWidth = 1;
 			c.stroke();
+		}
+
+		function animLoop() {
+			requestAnimFrame(animLoop);
+			canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+			$.each(userMissiles,function(i){
+				this.draw();
+			});
 		}
 
 
@@ -29,8 +44,9 @@
         		canvasElement.appendTo('main');
         		$(canvasElement).click(function(e){
         			var m = new Missile(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top);
-        			m.draw();
+        			userMissiles.push(m);
         		});
+        		requestAnimFrame(animLoop);
         	}
         }
 
